3b2fc2800b49240ab94ad55c35c1ac4ac05f3bcc
Paul R. Hibbing <paulhibbing@gmail.com>
2022-02-08 11:49:49 -0600

    Move data documentation to clean up R directory

--- R/0_Package_and_Data.R
+++ R/0_Package_and_Data.R
@@ -17,3 +17,12 @@ if(getRversion() >= "2.15.1") utils::globalVariables(c(
 #' @docType package
 #' @name PBpatterns
 NULL
+
+#' Data for use in code examples throughout the \code{PBpatterns} package
+#'
+#' This is a minute-by-minute data set for one participant in the 2003-2006
+#' National Health and Nutrition Examination Survey (NHANES). For details, see the
+#' \href{https://wwwn.cdc.gov/Nchs/Nhanes/2005-2006/PAXRAW_D.htm}{NHANES documentation}.
+#' (Link is to the 2005-2006 documentation, as an additional variable (PAXSTEP)
+#' was included in that cycle.)
+"example_data"
--- R/example_data.R
+++
@@ -1,8 +0,0 @@
-#' Data for use in code examples throughout the \code{PBpatterns} package
-#'
-#' This is a minute-by-minute data set for one participant in the 2003-2006
-#' National Health and Nutrition Examination Survey (NHANES). For details, see the
-#' \href{https://wwwn.cdc.gov/Nchs/Nhanes/2005-2006/PAXRAW_D.htm}{NHANES documentation}.
-#' (Link is to the 2005-2006 documentation, as an additional variable (PAXSTEP)
-#' was included in that cycle.)
-"example_data"
